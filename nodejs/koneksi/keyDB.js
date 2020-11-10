3cc9a22ec3e5b4b7f95cdd15bdff026bac11469f
Yunique333 <47499430+Yunique333@users.noreply.github.com>
2020-11-10 10:52:47 +0800

    Update keyDB.js

--- nodejs/koneksi/keyDB.js
+++ nodejs/koneksi/keyDB.js
@@ -1,4 +1,4 @@
 const mongoURI = {
-    suhu: 'mongodb+srv://cobadatabase:'
+    suhu: 'mongodb+srv://cobadatabase:<YourDBPassword>@cluster0-shard-00-00.sxr4g.gcp.mongodb.net:27017,cluster0-shard-00-01.sxr4g.gcp.mongodb.net:27017,cluster0-shard-00-02.sxr4g.gcp.mongodb.net:27017/Example?ssl=true&replicaSet=atlas-u07olz-shard-0&authSource=admin&retryWrites=true&w=majority'
 }
 module.exports = mongoURI;
